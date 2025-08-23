4ebe14bf944393b3005791bc63ec485e364f1453
ian <licitdev@gmail.com>
2025-08-24 00:09:06 +0800

    Add dark mode colors

--- docs/components/example/ExampleNested.vue
+++ docs/components/example/ExampleNested.vue
@@ -5,17 +5,17 @@ import { SplitPanel } from '@directus/vue-split-panel';
 <template>
 	<SplitPanel primary="start" class="h-128 w-full">
 		<template #start>
-			<div class="w-full h-full bg-orange-50 flex items-center justify-center">Panel A</div>
+			<div class="w-full h-full bg-orange-100 dark:bg-orange-900 flex items-center justify-center">Panel A</div>
 		</template>
 		
 		<template #end>
 			<SplitPanel orientation="vertical" class="h-full">
 				<template #start>
-					<div class="w-full h-full bg-red-50 flex items-center justify-center">Panel B</div>
+					<div class="w-full h-full bg-blue-100 dark:bg-blue-900 flex items-center justify-center">Panel B</div>
 				</template>
 				
 				<template #end>
-					<div class="w-full h-full bg-green-50 flex items-center justify-center">Panel C</div>
+					<div class="w-full h-full bg-green-100 dark:bg-green-900 flex items-center justify-center">Panel C</div>
 				</template>
 			</SplitPanel>
 		</template>
